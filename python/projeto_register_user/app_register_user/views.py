1d3919dcb879eba93eb6921b3895a9fee26289dc
Yuri Camilo <yr.camilo95@gmail.com>
2024-04-09 15:56:45 -0300

    Login Method Performed

--- python/projeto_register_user/app_register_user/views.py
+++ python/projeto_register_user/app_register_user/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 #Importa com nome de classe a tabela do SQL
 # Create your views here.
 
@@ -18,15 +20,15 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        user = User.objects.get(username=username)
-
-        if user:
-            return HttpResponse("Já existe um usuario com esse username")
+        if User.objects.filter(username=username).exists():
+            return HttpResponse("Já existe um usuário com este username.")
 
         user = User.objects.create_user(username=username,email=email,password=password)
         user.save()
-        return HttpResponse('Usuário Cadastrado com Sucesso')
+        return render(request, 'html/process.html')
      
+
+
 def login(request):
     if request.method == "GET":
         return render(request, 'html/login.html')
@@ -34,13 +36,16 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
+        # Autenticando usando o nome de usuário
+        user = authenticate(request, username=username, password=password)
         
         if user:
+            # Efetuando o login
             login_django(request, user)
-            return HttpResponse('válido')
+            return HttpResponse('Login válido')
         else:
-            return HttpResponse('Email ou Senha Inválido')
+            return HttpResponse('Nome de usuário ou senha inválidos')
+
 
 @login_required(login_url="/auth/login/")        
 def plataforma(request):
